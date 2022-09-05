d7c641310091c235e1132fc974893a4ca19d4be5
Ruben <r0830954@student.thomasmore.be>
2022-09-05 20:08:40 +0200

    contact page php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'rubencapota@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  
+   $contact->smtp = array(
+    'host' => 'smtp.elasticemail.com',
+   'username' => 'rubencapota@gmail.com',
+   'password' => 'C60B65CCF7E274DE3FD245DB178C3BFB4E33',
+   'port' => '2525'
+ );
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
