996d6ea0505339bfa9a9693e5da8c24adf05e8d5
Ruben <r0830954@student.thomasmore.be>
2022-09-11 13:47:09 +0200

    Changed SMTP

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-   $contact->smtp = array(
-    'host' => 'smtp.elasticemail.com',
-   'username' => 'rubencapota@gmail.com',
-   'password' => 'C60B65CCF7E274DE3FD245DB178C3BFB4E33',
-   'port' => '2525'
- );
+//   $contact->smtp = array(
+//     'host' => 'smtp.elasticemail.com',
+//    'username' => 'rubencapota@gmail.com',
+//    'password' => 'C60B65CCF7E274DE3FD245DB178C3BFB4E33',
+//    'port' => '2525'
+//  );
   
 
   $contact->add_message( $_POST['name'], 'From');
